319a86e0c7c6035b2a61f9cce7028d12b6b48358
LiXuFei <xfli@thoughtworks.com>
2016-11-14 17:50:51 +0800

    return a string response with spring boot.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,31 +5,12 @@ import hello.mappers.UserMapper;
 import hello.mappers.User;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-
-    static SqlSessionFactory sqlSessionFactory = null;
-    static {
-        sqlSessionFactory = MyBatisUtil.getSqlSessionFactory();
-    }
     public static void main(String[] args) {
-        testAdd();
-    }
-
-    private static void testAdd() {
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        try {
-            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-            User user = new User("xufei", new Integer(25));
-            userMapper.insert(user);
-            sqlSession.commit();// 这里一定要提交，不然数据进不去数据库中
-        } finally {
-            sqlSession.close();
-        }
+        SpringApplication.run(Application.class, args);
     }
 }
